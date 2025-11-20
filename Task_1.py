c949ff829330c6478881d57f4583fa1c8dc81d67
kdemchyshyn <kdemchyshyn@kse.org.ua>
2025-11-20 20:29:46 +0200

    Task 1 done

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,22 @@
 import numpy as np
+
+def getEigen(matrix):
+    if matrix.shape[0] != matrix.shape[1]:
+        return 1
+
+    result = list()
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+    for i in range(len(eigenvalues)):
+        value = eigenvalues[i]
+
+        Ax = matrix @ eigenvectors[:, i]
+        Vx = value * eigenvectors[:, i]
+
+        diff = Ax - Vx
+        if not np.allclose(diff, 0, 1e-10):
+            return 1
+
+        result.append((np.real(value), eigenvectors[i]))
+
+    return result
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import Task_1 as t1
 import Task_2 as t2
 import Task_3 as t3
+import numpy as np
 
 def main():
+    # Task 1
+    matrix = np.array([[-5, 0, 3], [-6, 1 ,3], [-6, 0, 4]])
+    eigen = t1.getEigen(matrix)
 
+    for el in eigen:
+        print(f"{el[0]} : {el[1]}")
+
+    #Task 2
     return 0
 
 if __name__ == '__main__':
